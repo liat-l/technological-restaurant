0e7d0c8dd2f846894550472c47db82f285994654
liat-l <liat1672@gmail.com>
2026-06-01 19:23:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ st.set_page_config(
     layout="wide"
 )
 
-# הגדרת דפי המערכת - דף בית ודף אופטימיזציה
 pages = {
     "מסך ראשי": [
-        st.Page("home_page.py", title="🏠 דף הבית", default=True)
+        st.Page("pages/home_page.py", title="🏠 דף הבית", default=True)
     ],
     "מודלים של אופטימיזציה": [
-        st.Page("set_cover_page.py", title="📍 פריסת רובוטים (Set Cover)")
+        st.Page("pages/set_cover_page.py", title="📍 פריסת רובוטים (Set Cover)")
     ]
+}
 }
 
 # הפעלת הניווט בין הדפים
