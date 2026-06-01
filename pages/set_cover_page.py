cd2f86f17c95a84cc874beded05760e457cb444e
liat-l <liat1672@gmail.com>
2026-06-01 20:23:41 +0300

    Update set_cover_page.py

--- pages/set_cover_page.py
+++ pages/set_cover_page.py
@@ -1,15 +1,17 @@
 # pages/set_cover_page.py
 import streamlit as st
 
-try:
-    from algorithms.set_cover import greedy_set_cover
-except ModuleNotFoundError:
-    from set_cover import greedy_set_cover
+# הגנת טעינה: אם משום מה הזיכרון הריק, נטען את ברירת המחדל
+if "DATA" not in st.session_state:
+    import data.restaurant_data as default_data
+    st.session_state["DATA"] = default_data
 
-# שליפת הנתונים הדינמיים מהזיכרון
+# שליפת הנתונים הדינמיים מהזיכרון הראשי
 data_source = st.session_state["DATA"]
 UNIVERSE = data_source.UNIVERSE
 SETS = data_source.SETS
 
 st.title("📍 אופטימיזציית כיסוי אזורים (Set Cover)")
-# ... שאר הקוד שלך נשאר בדיוק אותו הדבר ללא שינוי! ...
+st.write("מערכת המידע מפעילה אלגוריתם חמדני (Greedy Set Cover) למציאת כמות הרובוטים המינימלית הנדרשת.")
+
+# --- כאן שאר הקוד המקורי שלך (האלגוריתם, הציורים וההדפסות) ממשיך כרגיל ---
