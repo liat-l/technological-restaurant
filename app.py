c29b7f8e619340d88e1d716a974b40f9b3dd2787
liat-l <liat1672@gmail.com>
2026-06-01 19:55:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,16 @@ st.set_page_config(
     layout="wide"
 )
 
+# הגדרת דפי המערכת - כולל דף דייקסטרה החדש שהוספנו
 pages = {
     "מסך ראשי": [
         st.Page("pages/home_page.py", title="🏠 דף הבית", default=True)
     ],
     "מודלים של אופטימיזציה": [
-        st.Page("pages/set_cover_page.py", title="📍 פריסת רובוטים (Set Cover)")
+        st.Page("pages/set_cover_page.py", title="📍 פריסת רובוטים (Set Cover)"),
+        st.Page("pages/dijkstra_page.py", title="⚡ מסלולים קצרים (Dijkstra)")
     ]
 }
-
 
 # הפעלת הניווט בין הדפים
 pg = st.navigation(pages)
